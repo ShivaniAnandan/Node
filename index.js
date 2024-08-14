8e082495a9b3be1f24e7e4a32ff5afce796be2a2
ShivaniAnandan <shivanianandhan28@gmail.com>
2024-08-14 15:01:03 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+//old way of using express used in ES6
+// const express = require('express');
+
+import express from 'express';
+
+const app = express();
+
+const PORT = 4000;
+
+// app.use() //Middleware
+
+// app.get('/'); 
+
+// app.post('/products');
+
+app.get('/', (req,res) => {
+    res.status(200).json({message:"Hi my dear friends"})
+})
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running in the ${PORT}`);
+}); //running
